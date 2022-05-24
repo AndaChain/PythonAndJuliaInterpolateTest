b900b381ddfa00524abd33611c94ce96739c5d1c
AndaChain <andaanda789@gmail.com>
2022-05-24 17:15:52 +0700

    add quadratic interpolate

--- Interpolate_DIY.py
+++ Interpolate_DIY.py
@@ -1,10 +1,6 @@
-#Numpy
-import datetime
 import numpy as np
 import matplotlib.pyplot as plt
-#import scipy.interpolate as interpolate
 from scipy.interpolate import interp1d
-#from scipy.interpolate import lagrange
 from sympy import *
 
 def fx(x):
@@ -13,15 +9,55 @@ def fx(x):
 class Interpolation():
     def __init__(self):
         self.result = []
-        
     def linear_function(self,x,y) :
+        self.result = []
         X = Symbol('x')
         for i in range(1,len(x)) :
             a1 = y[i-1]/(x[i-1]-x[i])
             a2 = y[i]/(x[i]-x[i-1])
             self.result.append([(a1*(X-x[i]))+(a2*(X-x[i-1])),x[i-1],x[i]])
-            
-    def set_linear(self,x):
+
+    #----------------------------------------------------------------------------
+
+    def quadratic_function(self,x,y) :
+        self.result = []
+        a_initial = 0
+        b_initial = 0
+        X = Symbol('x')
+        for i in range(1,len(x)) :
+            if i == 1 :
+                matrix_A = np.array([[x[i-1],1],[x[i],1]])
+                matrix_B = np.array([y[i-1],y[i]])
+                output = np.linalg.solve(matrix_A,matrix_B)
+                b_initial = output[0]
+                equation = (b_initial*X) + output[1]
+                self.result.append([equation,x[i-1],x[i]])
+            elif i == 2 :
+                row1 = [2*x[i-1],1,0]
+                row2 = [(x[i-1])**2,x[i-1],1]
+                row3 = [(x[i])**2,x[i],1]
+                matrix_A = np.array([row1,row2,row3])
+                matrix_B = np.array([b_initial,y[i-1],y[i]])
+                output = np.linalg.solve(matrix_A,matrix_B)
+                a_initial = output[0]
+                b_initial = output[1]
+                equation = (a_initial*(X**2)) + (b_initial*X) + output[2]
+                self.result.append([equation,x[i-1],x[i]])
+            else:
+                row1 = [2*x[i-1],1,0]
+                row2 = [(x[i-1])**2,x[i-1],1]
+                row3 = [(x[i])**2,x[i],1]
+                matrix_A = np.array([row1,row2,row3])
+                matrix_B = np.array([(2*a_initial*x[i-1]) + b_initial,y[i-1],y[i]])
+                output = np.linalg.solve(matrix_A,matrix_B)
+                a_initial = output[0]
+                b_initial = output[1]
+                equation = (a_initial*(X**2)) + (b_initial*X) + output[2]
+                self.result.append([equation,x[i-1],x[i]])
+
+    #----------------------------------------------------------------------------
+
+    def set_function(self,x):
         X = Symbol('x')
         y = []
         for i in x :
@@ -31,19 +67,34 @@ class Interpolation():
                     y.append(f.subs({X:i}))
                     break
         return y
+
+    #----------------------------------------------------------------------------
         
 def main():
     start = -2*np.pi
     stop = 2*np.pi
-    step = 1000
+    step = 10
     x = np.linspace(start, stop, num=step, endpoint=True)
     y = fx(x)
     
+    #-----------------------linear------------------------------
     linear_interpolated = Interpolation()
     interpo = linear_interpolated.linear_function(x,y)
 
     new_x = np.linspace(start, stop, num=step*10, endpoint=True)
-    function_y =  linear_interpolated.set_linear(new_x)
+    function_y =  linear_interpolated.set_function(new_x)
 
+    #-----------------------quadratic------------------------------
+    quadratic_interpolated = Interpolation()
+    interpo2 = quadratic_interpolated.quadratic_function(x,y)
+
+    function_y2 =  quadratic_interpolated.set_function(new_x)
+
+    f3 = interp1d(x, y, kind='quadratic')
+
+    #-----------------------Plot------------------------------
+    plt.plot(x, y, 'o', new_x, f3(new_x), '-', new_x, function_y2, '--')
+    plt.legend(['data', 'AI_quadratic', 'quadratic'], loc='best')
+    plt.show()
 
 main()
